e724e093317eab91f67a6b1dfda469194543babd
Lucas <lucas14guesser@gmail.com>
2023-10-05 21:44:15 -0300

    render

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.post('/formulario', (req, res) => {
     });
 });
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log("Servidor rodando!" );
 })
